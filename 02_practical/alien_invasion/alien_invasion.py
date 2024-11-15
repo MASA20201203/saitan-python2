19c0e5623ac64816118346e933aad015fee7b930
MASA20201203 <koedame032@gmail.com>
2024-11-16 03:10:30 +0900

    update: p34 発射できる弾の上限を3発に設定

--- 02_practical/alien_invasion/alien_invasion.py
+++ 02_practical/alien_invasion/alien_invasion.py
@@ -67,8 +67,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """新しい弾を生成し bullets グループに追加する"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """画面上の画像を更新し、新しい画面に切り替える"""
