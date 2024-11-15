b7aa3ff6e328e8bea3cc368423ead6aea0ccd56b
MASA20201203 <koedame032@gmail.com>
2024-11-15 21:38:30 +0900

    update: p8 フレームレートを設定

--- 02_practical/alien_invasion/alien_invasion.py
+++ 02_practical/alien_invasion/alien_invasion.py
@@ -8,6 +8,7 @@ class AlienInvasion:
     def __init__(self):
         """ゲームを初期化し、ゲームのリソースを作成する"""
         pygame.init()
+        self.clock = pygame.time.Clock()
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("エイリアン侵略")
@@ -22,6 +23,7 @@ class AlienInvasion:
 
             # 最新の画面を表示する
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # ゲームのインスタンスを作成し、ゲームを実行する
