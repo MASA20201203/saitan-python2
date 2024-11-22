8516b99687b8d99173f5d6f5d8affd2fbefe8f73
MASA20201203 <koedame032@gmail.com>
2024-11-23 06:57:59 +0900

    update: p161 ホバー時に大体の位置を表示させる

--- 02_practical/chap05/eq_explore_data.py
+++ 02_practical/chap05/eq_explore_data.py
@@ -11,20 +11,23 @@ all_eq_data = json.loads(contents)
 # データセットにある全ての地震を調べる
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    eq_titles.append(title)
 
 title = '世界の地震'
 fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title,
         color=mags,
         color_continuous_scale='Viridis',
         labels={'color': 'マグニチュード'},
-        projection='natural earth'
+        projection='natural earth',
+        hover_name=eq_titles,
     )
 fig.show()
