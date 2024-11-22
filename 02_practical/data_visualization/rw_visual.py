e649db30ccc6470786b6bebbb5e575913fe51a53
MASA20201203 <koedame032@gmail.com>
2024-11-22 22:29:01 +0900

    update: p118 開始点と終了点を強調

--- 02_practical/data_visualization/rw_visual.py
+++ 02_practical/data_visualization/rw_visual.py
@@ -15,6 +15,12 @@ while True:
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
             edgecolors='none', s=15)
     ax.set_aspect('equal')
+
+    # 開始点と終了点を強調する
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+            s=100)
+
     plt.show()
 
     keep_running = input("別のランダムウォークを生成しますか？ (y/n): ")
