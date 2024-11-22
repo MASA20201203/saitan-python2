44c55e57370afb36b5be3ec60bff806a658c5b23
MASA20201203 <koedame032@gmail.com>
2024-11-23 05:38:30 +0900

    update: p143 最低気温も表示

--- 02_practical/chap05/sitka_highs.py
+++ 02_practical/chap05/sitka_highs.py
@@ -11,20 +11,23 @@ render = csv.reader(lines)
 header_row = next(render)
 
 # 日付と最高気温を取り出す
-dates, highs = [], []
+dates, highs, lows = [], [], []
 for row in render:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
 # 最高気温のグラフを描画する
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
+ax.plot(dates, lows, c='blue')
 
 # グラフのフォーマット設定
-ax.set_title('Daily high temperatures, 2021', fontsize=24)
+ax.set_title('Daily High and Low Temperatures, 2021', fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('Temperature (F)', fontsize=16)
