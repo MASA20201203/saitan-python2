a1f2c2dfec104a14eb2d97f0dfeda08a8efa4d7f
MASA20201203 <koedame032@gmail.com>
2024-11-23 05:26:22 +0900

    update: p138 最高気温のデータをグラフ化

--- 02_practical/chap05/sitka_highs.py
+++ 02_practical/chap05/sitka_highs.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import csv
 
+import matplotlib.pyplot as plt
+
 path = Path('weather_data/sitka_weather_07-2021_simple.csv')
 lines = path.read_text().splitlines()
 
@@ -13,4 +15,15 @@ for row in render:
     high = int(row[4])
     highs.append(high)
 
-print(highs)
+# 最高気温のグラフを描画する
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# グラフのフォーマット設定
+plt.title('Daily high temperatures, July 2021', fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel('Temperature (F)', fontsize=16)
+plt.tick_params(labelsize=16)
+
+plt.show()
