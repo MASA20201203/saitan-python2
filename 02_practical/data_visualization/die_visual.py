102913ccf3872691ba13dff714755f1adb7aa83f
MASA20201203 <koedame032@gmail.com>
2024-11-22 22:56:58 +0900

    update: p124 結果を分析する

--- 02_practical/data_visualization/die_visual.py
+++ 02_practical/data_visualization/die_visual.py
@@ -5,8 +5,15 @@ die = Die()
 
 # サイコロを転がし、結果をリストに格納する
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# 結果を分析する
+frequencies = []
+poss_results = range(1, die.num_sides + 1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
