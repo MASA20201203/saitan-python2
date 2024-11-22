3b677fd586e42ed155ce4ef0e2631a2ae1e70013
MASA20201203 <koedame032@gmail.com>
2024-11-23 05:54:24 +0900

    add: p147 エラーチェックのためにデス・バレーのcsvを読み込んで表示

---
+++ 02_practical/chap05/death_valley_highs_lows.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+path = Path('weather_data/death_valley_2021_simple.csv')
+lines = path.read_text().splitlines()
+
+render = csv.reader(lines)
+header_row = next(render)
+
+# 日付と最高気温を取り出す
+dates, highs, lows = [], [], []
+for row in render:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    try:
+        high = int(row[3])
+        low = int(row[4])
+    except ValueError:
+        print(f"{current_date} のデータが存在しません")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+# 最高気温のグラフを描画する
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# グラフのフォーマット設定
+ax.set_title('Daily High and Low Temperatures, 2021\nDeath Valley, CA')
+ax.set_xlabel('', fontsize=20)
+fig.autofmt_xdate()
+ax.set_ylabel('Temperature (F)', fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
