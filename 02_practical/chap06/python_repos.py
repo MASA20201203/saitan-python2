9bc71ff37f6631aec3c2ceb90e86879423e04357
MASA20201203 <koedame032@gmail.com>
2024-11-23 07:30:37 +0900

    update: p170 レスポンスの辞書を処理する

--- 02_practical/chap06/python_repos.py
+++ 02_practical/chap06/python_repos.py
@@ -2,7 +2,7 @@ import requests
 
 # API呼び出しを作成してレスポンスを確認する
 url = "https://api.github.com/search/repositories"
-url += "?q=language:python+sort:stars:>10000"
+url += "?q=language:python+sort:stars+stars:>10000"
 
 headers = {"Accept": "application/vnd.github.v3+json"}
 r = requests.get(url, headers=headers)
@@ -11,5 +11,21 @@ print(f"ステータスコード: {r.status_code}")
 # レスポンスのオブジェクトを辞書に変換する
 response_dict = r.json()
 
-# 結果を処理する
-print(response_dict.keys())
+print(f"合計リポジトリ数: {response_dict['total_count']}")
+print(f"完全な結果: {not response_dict['incomplete_results']}")
+
+# リポジトリについての情報を調べる
+repo_dicts = response_dict["items"]
+print(f"情報が返されたリポジトリ数: {len(repo_dicts)}")
+
+# 1つ目のリポジトリについて調べる
+repo_dict = repo_dicts[0]
+
+print("\n1つ目のリポジトリ")
+print(f"名前: {repo_dict['name']}")
+print(f"所有者: {repo_dict['owner']['login']}")
+print(f"スター数: {repo_dict['stargazers_count']}")
+print(f"リポジトリ: {repo_dict['html_url']}")
+print(f"作成日時: {repo_dict['created_at']}")
+print(f"最終更新日時: {repo_dict['updated_at']}")
+print(f"説明: {repo_dict['description']}")
