c8ae8ddf04ba208838f79087544b385189d90446
MASA20201203 <koedame032@gmail.com>
2024-11-22 21:25:41 +0900

    update: p104 組み込みスタイルを使用

--- 02_practical/data_visualization/mpl_squares.py
+++ 02_practical/data_visualization/mpl_squares.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 input_value = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
+plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(input_value, squares, linewidth=3)
 
