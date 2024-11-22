d1c1e6a86033e10b683f3927bafd3e6baa9f3680
MASA20201203 <koedame032@gmail.com>
2024-11-22 21:32:52 +0900

    update: p105 グラフを修正

--- 02_practical/data_visualization/scatter_squares.py
+++ 02_practical/data_visualization/scatter_squares.py
@@ -2,6 +2,13 @@ import matplotlib.pyplot as plt
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.scatter(2, 4)
+ax.scatter(2, 4, s=300)
 
+# グラフのタイトルと軸ラベルを設定する
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# 目盛りラベルのサイズを設定する
+ax.tick_params(labelsize=14)
 plt.show()
