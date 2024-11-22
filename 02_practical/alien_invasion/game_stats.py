23ccd3b9c4a2f530e23fcc5dd644b9735b7de8eb
MASA20201203 <koedame032@gmail.com>
2024-11-22 18:40:06 +0900

    update: p86 ハイスコアを表示する

--- 02_practical/alien_invasion/game_stats.py
+++ 02_practical/alien_invasion/game_stats.py
@@ -6,6 +6,9 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        # ハイスコアはリセットしない
+        self.high_score = 0
+
     def reset_stats(self):
         """ゲーム中に変更される統計情報を初期化する"""
         self.ships_left = self.settings.ship_limit
