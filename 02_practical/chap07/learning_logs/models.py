9fefb8bcdbe8f416bc760f0649928cd063975e64
MASA20201203 <koedame032@gmail.com>
2024-11-27 01:26:42 +0900

    update: p205 50文字以下の文字には...を表示させない

--- 02_practical/chap07/learning_logs/models.py
+++ 02_practical/chap07/learning_logs/models.py
@@ -21,4 +21,7 @@ class Entry(models.Model):
 
     def __str__(self):
         """モデルの文字列表現を返す"""
-        return f"{self.text[:50]}..."
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return self.text
