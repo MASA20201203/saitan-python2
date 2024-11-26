a96466438a8c350a8fa68ec0528104ccc4bca091
MASA20201203 <koedame032@gmail.com>
2024-11-27 06:15:37 +0900

    update: p226 新しいトピックを追加_new_topic()ビュー関数を追加

--- 02_practical/chap07/learning_logs/views.py
+++ 02_practical/chap07/learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
     """学習ノートのホームページ"""
@@ -18,3 +19,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """新しいトピックを追加する"""
+    if request.method != 'POST':
+        # データが送信されていないので空のフォームを作成する
+        form = TopicForm()
+    else:
+        # POSTデータが送信されたのでデータを処理する
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # 空または無効なフォームを表示する
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
